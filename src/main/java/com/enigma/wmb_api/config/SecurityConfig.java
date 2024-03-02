01ebab08f2f23c4edcb3e6ab9183468d0407e88b
Muhammad Irfan Shiddiq <irfams99@gmail.com>
2024-03-02 13:59:04 +0700

    [FEATURE] Add SecurityConfig

---
+++ src/main/java/com/enigma/wmb_api/config/SecurityConfig.java
@@ -0,0 +1,24 @@
+package com.enigma.wmb_api.config;
+
+import jakarta.servlet.DispatcherType;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests((requests) -> {
+                    requests.dispatcherTypeMatchers(DispatcherType.ERROR).permitAll()
+                            .anyRequest().permitAll();
+                })
+                .build();
+    }
+
+}
